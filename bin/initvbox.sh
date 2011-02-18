ae2539c92d04fb6b6662d3b8f4fadc5496fe6a9f
Marco Moreira <talk2marco@gmail.com>
2011-02-18 14:47:05 -0500

    Initial commit

---
+++ bin/initvbox.sh
@@ -0,0 +1,23 @@
+#!/django-starter bash
+cd $PWD
+
+
+#if [ ! -f $HOME/bin/upload.py ]
+#then
+#wget -O $HOME/bin/upload.py  http://codereview.appspot.com/static/upload.py
+#chmod +x $HOME/bin/upload.py
+#fi
+
+
+echo "Installing the basics"
+curl -O http://python-distribute.org/distribute_setup.py
+sudo python distribute_setup.py
+sudo easy_install pip
+sudo pip install virtualenv
+sudo pip install virtualenvwrapper
+
+echo "Populating virtual environment"
+cd ..
+virtualenv env --no-site-packages -p python2.6
+#source env/bin/activate
+env/bin/pip install -r requirements.txt
